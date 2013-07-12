0a54c0ec2ca608027c25a057ba34c53fcf706a63
Ricardo Quesada <ricardoquesada@gmail.com>
2013-07-11 17:00:33 -0700

    Adds iOS and Mac builds

---
+++ build-ios/build.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+
+MIN_IOS_VERSION=4.3
+
+SDK_PATH=`xcrun -sdk iphoneos --show-sdk-path`
+
+CC=`xcrun -sdk iphoneos -find clang`
+LD=`xcrun -sdk iphoneos -find ld`
+AR=`xcrun -sdk iphoneos -find ar`
+LIPO="xcrun -sdk iphoneos lipo"
+cpus=$(sysctl hw.ncpu | awk '{print $2}')
+
+#
+# 32 bit
+#
+../configure --without-png CFLAGS="-arch i386"
+make clean
+make -j$cpus
+cp .libs/libfreetype.a libfreetype-i386.a
+
+#
+# armv7
+#
+../configure --host=arm-apple-darwin --enable-static=yes --enable-shared=no --without-png \
+	CPPFLAGS="-arch armv7 -fpascal-strings -Os -fmessage-length=0 -fvisibility=hidden -miphoneos-version-min=$MIN_IOS_VERSION -I$SDK_PATH/usr/include/libxml2 -isysroot $SDK_PATH" \
+	CFLAGS="-arch armv7 -fpascal-strings -Os -fmessage-length=0 -fvisibility=hidden -miphoneos-version-min=$MIN_IOS_VERSION -I$SDK_PATH/usr/include/libxml2 -isysroot $SDK_PATH" \
+	LDFLAGS="-arch armv7 -isysroot $SDK_PATH -miphoneos-version-min=$MIN_IOS_VERSION"
+
+make clean
+make -j$cpus
+cp .libs/libfreetype.a libfreetype-armv7.a
+
+#
+# armv7s
+#
+../configure --host=arm-apple-darwin --enable-static=yes --enable-shared=no --without-png \
+	CPPFLAGS="-arch armv7s -fpascal-strings -Os -fmessage-length=0 -fvisibility=hidden -miphoneos-version-min=$MIN_IOS_VERSION -I$SDK_PATH/usr/include/libxml2 -isysroot $SDK_PATH" \
+	CFLAGS="-arch armv7s -fpascal-strings -Os -fmessage-length=0 -fvisibility=hidden -miphoneos-version-min=$MIN_IOS_VERSION -I$SDK_PATH/usr/include/libxml2 -isysroot $SDK_PATH" \
+	LDFLAGS="-arch armv7s -isysroot $SDK_PATH -miphoneos-version-min=$MIN_IOS_VERSION"
+
+make clean
+make -j$cpus
+cp .libs/libfreetype.a libfreetype-armv7s.a
+#
+#
+# lib
+lipo -create -output libfreetype.a libfreetype-i386.a libfreetype-armv7.a libfreetype-armv7s.a
+
+# print info
+lipo -info libfreetype.a
---
+++ build-mac/build.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+#
+# 64 bit
+#
+../configure --without-png CFLAGS="-arch x86_64"
+make clean; make
+cp .libs/libfreetype.a libfreetype-x86_64.a
+
+
+#
+# 32 bit
+#
+../configure --without-png CFLAGS="-arch i386"
+make clean; make 
+cp .libs/libfreetype.a libfreetype-i386.a
+
+
+# lib
+lipo -create -output libfreetype.a libfreetype-i386.a libfreetype-x86_64.a
+
+# print info
+lipo -info libfreetype.a
